14a2c2d8062d75eb0713a85dd8bace51ba70f671
unknown <jegq.dev.projects@gmail.com>
2023-04-11 16:09:29 -0400

    Implementeación de code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
+import { lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import axios from "axios";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
-import SignupPage from "./pages/SignupPage";
-import MapPage from "./pages/MapPage";
+
 import ReconnectUser from "./components/ReconnectUser";
+import Spinner from "./components/Spinner";
 import store from "./redux/store";
 import "./index.css";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const SignupPage = lazy(() => import("./pages/SignupPage"));
+const MapPage = lazy(() => import("./pages/MapPage"));
 
 axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <main className="min-h-screen bg-slate-100">
-        <ReconnectUser />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />}  />
-            <Route path="/map" element={<MapPage />}  />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="*" element={<h1>Page not found...</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </main>
-    </Provider>
+    <Suspense fallback={<Spinner size="large" />}>
+      <Provider store={store}>
+        <main className="min-h-screen bg-slate-100">
+          <ReconnectUser />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />}  />
+              <Route path="/map" element={<MapPage />}  />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="*" element={<h1>Page not found...</h1>} />
+            </Routes>
+          </BrowserRouter>
+        </main>
+      </Provider>
+    </Suspense>
   )
 };
 
