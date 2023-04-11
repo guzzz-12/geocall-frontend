550dab74b3586033d750bd913e6dd097c5cd0ebc
unknown <jegq.dev.projects@gmail.com>
2023-04-11 10:43:58 -0400

    Refactorización de la funcionalidad de socket al reconectar el usuario al servidor

--- src/components/ReconnectUser.tsx
+++ src/components/ReconnectUser.tsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import useGetUserLocation from "../hooks/useGetUserLocation";
 import { socketClient, SocketEvents } from "../socket/socketClient";
 import { useGetUserQuery } from "../redux/api";
 import { OnlineUser, setOnlineUsers } from "../redux/features/mapSlice";
+import { MapRootState } from "../redux/store";
 
 /**
  * Reconectar el usuario al servidor de websocket,
@@ -13,23 +14,28 @@ import { OnlineUser, setOnlineUsers } from "../redux/features/mapSlice";
  */
 const ReconnectUser = () => {  
   const dispatch = useDispatch();
+  const {myLocation} = useSelector((state: MapRootState) => state.map);
+
   const {data: userData} = useGetUserQuery();
 
   // Obtener la ubicación del usuario
   useGetUserLocation();
 
   useEffect(() => {
-    if (userData) {
-      // Reconectar el usuario al servidor de socket
-      socketClient.userReconnected(userData._id); 
+    if (userData && myLocation) {
+      // Agregar/actualizar el usuario en la lista de
+      // los usuarios online del servidor de socket
+      // al autenticarse o actualizar la página
+      socketClient.userReconnected(userData._id, myLocation); 
 
       // Escuchar evento de usuarios online
       // para actualizar el state en tiempo real
       socketClient.socket.on(SocketEvents.GET_ONLINE_USERS, (users: OnlineUser[]) => {
+        console.log({onlineUsers: users});
         dispatch(setOnlineUsers(users));
       });
     };
-  }, [userData]);
+  }, [userData, myLocation]);
 
   return null;
 };
