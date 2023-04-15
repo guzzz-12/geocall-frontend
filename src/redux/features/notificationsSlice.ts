9c032ed16a08256638c8f8a587f936350a7c7d39
unknown <jegq.dev.projects@gmail.com>
2023-04-15 10:56:13 -0400

    State global de las notificaciones

---
+++ src/redux/features/notificationsSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface Notification {
+  notificationId: string;
+  notificationType: "incomingMessage" | "incomingVideocall";
+  senderId: string;
+  senderData: {
+    firstName: string;
+    lastName: string;
+    avatar: string;
+  };
+  senderSocketId: string;
+  receiverId: string;
+  receiverSockerId: string;
+  unread: boolean;
+};
+
+export interface NotificationsState {
+  unread: Notification[];
+  all: Notification[];
+};
+
+const initialState: NotificationsState = {
+  unread: [],
+  all: []
+};
+
+const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {
+    setNotifications: (state, action: {type: string, payload: Notification}) => {
+      state.all = [action.payload, ...state.all];
+      state.unread = [action.payload, ...state.unread];
+    },
+    setReadNotifications: (state) => {
+      const readNotifications = [...state.all].map((notification) => {
+        notification.unread = false;
+        return notification;
+      });
+
+      state.all = readNotifications;
+      state.unread = [];
+    }
+  }
+});
+
+export const notificationsReducer = notificationsSlice.reducer;
+export const { setNotifications, setReadNotifications } = notificationsSlice.actions;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { MapState, mapReducer } from "./features/mapSlice";
 import { UserState, userReducer } from "./features/userSlice";
 import { ChatsState, chatsReducer } from "./features/chatsSlice";
+import { NotificationsState, notificationsReducer } from "./features/notificationsSlice";
 import { api } from "./api";
 
 export interface MapRootState {
@@ -16,11 +17,16 @@ export interface ChatsRootState {
   chats: ChatsState;
 };
 
+export interface NotificationsRootState {
+  notifications: NotificationsState
+};
+
 const store = configureStore({
   reducer: {
     map: mapReducer,
     user: userReducer,
     chats: chatsReducer,
+    notifications: notificationsReducer,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
