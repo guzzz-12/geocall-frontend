a9a9f44241e4418aca0d45b75162c963056923c8
unknown <jegq.dev.projects@gmail.com>
2023-04-17 16:50:55 -0400

    Custom hook para solicitar permisos de acceso a la cámara y almacenar el stream local en el state global

---
+++ src/hooks/useMediaDevices.tsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setLocalStream } from "../redux/features/videoCallSlice";
+
+/**
+ * Custom hook para solicitar los permisos de acceso a la cámara
+ * al intentar realizar una videollamada
+ */
+const useMediaDevices = () => {
+  const dispatch = useDispatch();
+
+  const [myStream, _setMyStream] = useState<MediaStream | null>(null);
+  const [streamError, setStreamError] = useState<string | null>(null);
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    })
+    .then((stream) => {
+      setStreamError(null);
+      dispatch(setLocalStream(stream));
+    })
+    .catch((err: any) => {
+      setStreamError(err.message);
+    });
+  }, []);
+
+  return {myStream, streamError};
+};
+
+export default useMediaDevices;
