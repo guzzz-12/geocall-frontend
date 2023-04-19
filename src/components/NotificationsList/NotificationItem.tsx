bf4772694b5313c026e15b678322eb10d03d011c
unknown <jegq.dev.projects@gmail.com>
2023-04-19 11:22:20 -0400

    Abrir la ventana del chat al clickear la notificación

--- src/components/NotificationsList/NotificationItem.tsx
+++ src/components/NotificationsList/NotificationItem.tsx
@@ -1,4 +1,9 @@
+import { v4 } from "uuid";
+import { useSelector, useDispatch } from "react-redux";
+import { Chat, createOrSelectChat } from "../../redux/features/chatsSlice";
 import { Notification } from "../../redux/features/notificationsSlice"
+import { setSelectedUserPrefetch } from "../../redux/features/mapSlice";
+import { UserRootState } from "../../redux/store";
 
 interface Props {
   notification: Notification;
@@ -7,10 +12,34 @@ interface Props {
 const NotificationItem = ({notification}: Props) => {
   const {senderData: {firstName, avatar}, notificationType, unread} = notification;
 
+  const dispatch = useDispatch();
+  const {currentUser} = useSelector((state: UserRootState) => state.user);
+
+  /**
+   * Seleccionar el chat al clickear la notificación correspondiente
+   */
+  const onClickNotificationHandler = () => {
+    const chat: Chat = {
+      chatId: v4(),
+      senderId: notification.senderId,
+      recipientId: currentUser!._id,
+      messages: [],
+      createdAt: new Date().toISOString()
+    };
+    
+    dispatch(setSelectedUserPrefetch({
+      selectedUserId: notification.senderId,
+      selectedUserSocketId: notification.senderSocketId
+    }));
+
+    dispatch(createOrSelectChat(chat));
+  };
+
   return (
     <div
       style={{backgroundColor: unread ? "#e0f2fe" : "transparent"}}
       className="flex justify-start items-center gap-2 w-full px-2 py-2 border-b border-gray-200 cursor-pointer transition-all"
+      onClick={onClickNotificationHandler}
     >
       <div className="w-10 h-10 flex-shrink-0 rounded-full border border-gray-300 overflow-hidden">
         <img
