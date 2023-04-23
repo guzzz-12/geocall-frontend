2d22309902f823b303223128c446799460b313f8
unknown <jegq.dev.projects@gmail.com>
2023-04-22 20:45:00 -0400

    Tomar la información del usuario en el chat item de la data estática en lugar de consultarla en la API

--- src/components/ChatsList/ChatItem.tsx
+++ src/components/ChatsList/ChatItem.tsx
@@ -1,5 +1,4 @@
 import { useSelector, useDispatch } from "react-redux";
-import useSelectedUser from "../../hooks/useSelectedUser";
 import { Chat, createOrSelectChat, setReadMessages } from "../../redux/features/chatsSlice";
 import { UserRootState } from "../../redux/store";
 
@@ -11,17 +10,14 @@ const ChatItem = ({chat}: Props) => {
   const dispatch = useDispatch();
   const {currentUser} = useSelector((state: UserRootState) => state.user);
 
-  // Extraer la ID del otro usuario de la conversación
+  // Extraer la ID y la data del otro usuario de la conversación
   const otherUserId = chat.senderId === currentUser?._id ? chat.recipientId : chat.senderId;
+  const otherUserData = chat.senderId === currentUser?._id ? chat.recipientData : chat.senderData;
 
   // Buscar los mensajes sin leer del otro usuario
   const unreadMessages = chat.messages.filter((msg) => msg.senderId === otherUserId && msg.unread);
 
-  // Consultar la data del usuario seleccionado y actualizar el state global
-  const {data, isLoading, isFetching} = useSelectedUser({selectedUserId: otherUserId});
-
-
-  if (!currentUser) {
+  if (!currentUser || !otherUserData) {
     return null
   };
 
@@ -41,33 +37,23 @@ const ChatItem = ({chat}: Props) => {
       className="flex justify-start items-center gap-2 w-full px-2 py-2 border-b border-gray-200 cursor-pointer transition-all overflow-hidden"
       onClick={onClickChatHandler}
     >
-      {(isLoading || isFetching) && (
-        <>
-          <div className="animate-pulse w-10 h-10 flex-shrink-0 rounded-full border border-gray-500 bg-gray-300"/>
-          <div className="animate-pulse flex-grow h-[40px] bg-gray-300" />
-        </>
-      )}
 
-      {!isLoading && !isFetching && (
-        <>
-          <div className="w-10 h-10 flex-shrink-0 rounded-full border border-gray-300 overflow-hidden">
-            <img
-              className="block w-full h-full object-cover object-center"
-              src={data?.user.avatar}
-              alt={data?.user.firstName}
-            />
-          </div>
+      <div className="w-10 h-10 flex-shrink-0 rounded-full border border-gray-300 overflow-hidden">
+        <img
+          className="block w-full h-full object-cover object-center"
+          src={otherUserData.avatar}
+          alt={otherUserData.firstName}
+        />
+      </div>
 
-          <p className="flex-grow max-w-[full] text-sm text-left text-gray-700 text-ellipsis overflow-hidden">
-            Chat with <span className="font-bold">{data?.user.firstName}</span>
-          </p>
+      <p className="flex-grow max-w-[full] text-sm text-left text-gray-700 text-ellipsis overflow-hidden">
+        Chat with <span className="font-bold">{otherUserData.firstName}</span>
+      </p>
 
-          {unreadMessages.length > 0 && (
-            <p className="flex justify-center items-center w-5 h-5 ml-auto text-sm text-white font-semibold rounded-full bg-orange-700">
-              {unreadMessages.length}
-            </p>
-          )}
-        </>
+      {unreadMessages.length > 0 && (
+        <p className="flex justify-center items-center w-5 h-5 ml-auto text-sm text-white font-semibold rounded-full bg-orange-700">
+          {unreadMessages.length}
+        </p>
       )}
     </div>
   )
