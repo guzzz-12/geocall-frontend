f9f180b4af2a3c1946da986821da6dbaec4f84a8
unknown <jegq.dev.projects@gmail.com>
2023-04-24 22:16:04 -0400

    Proteger las rutas con autenticación y verificación

---
+++ src/components/HOC/withVerification.tsx
@@ -0,0 +1,29 @@
+import { ComponentType } from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { UserRootState } from "../../redux/store";
+
+/**
+ * HOC para proteger las rutas que requieran
+ * autenticación y verificación.
+ */
+const withVerification = (Component: ComponentType) => {
+  return () => {
+    const {isLoading, currentUser}= useSelector((state: UserRootState) => state.user);
+    
+    // Si no está autenticado, redirigir a la página de login
+    if(!isLoading && !currentUser) {
+      return <Navigate to="/login" replace />
+    };
+
+    // Si está autenticado pero no está verificado,
+    // redirigir a la página de verificación
+    if (!isLoading && currentUser && !currentUser.emailVerified) {
+      return <Navigate to="/verify-account" replace />
+    };
+
+    return <Component />;
+  };
+};
+
+export default withVerification;
