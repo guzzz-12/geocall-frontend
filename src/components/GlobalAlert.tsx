b5871f710882637023ddd01c075d25904b23fa6b
unknown <jegq.dev.projects@gmail.com>
2023-05-04 19:35:17 -0400

    Corrección del posicionamiento de global alert, no mostrar el global alert si no está autenticado

--- src/components/GlobalAlert.tsx
+++ src/components/GlobalAlert.tsx
@@ -1,18 +1,23 @@
 import { useSelector } from "react-redux";
 import { AnimatePresence, motion} from "framer-motion";
-import { GlobalAlertRootState } from "../redux/store";
+import { GlobalAlertRootState, UserRootState } from "../redux/store";
 
 const GlobalAlert = () => {
+  const {isAuth} = useSelector((state: UserRootState) => state.user);
   const {isOpen, message} = useSelector((state: GlobalAlertRootState) => state.globalAlert);
+
+  if (!isAuth) {
+    return null
+  };
 
   return (
     <AnimatePresence>
       {isOpen && message &&
         <motion.div
-          className="fixed bottom-4 left-[50%] flex justify-center items-center min-w-[200px] max-w-[400px] px-3 py-2 -translate-x-[50%] bg-red-50 border-2 border-red-700 rounded-md"
-          initial={{y: "100%", opacity: 0}}
-          animate={{y: 0, opacity: 1}}
-          exit={{y: "130%", opacity: 0}}
+          className="fixed bottom-2 left-[50%] flex justify-center items-center min-w-[200px] max-w-[400px] px-3 py-2 bg-red-50 border-2 border-red-700 rounded-md"
+          initial={{x: "-50%", y: "100%", z: 0, opacity: 0}}
+          animate={{x: "-50%", y: 0, z: 0, opacity: 1}}
+          exit={{x: "-50%", y: "130%", z: 0, opacity: 0}}
         >
           <p
             className="text-center text-gray-700 font-normal"
